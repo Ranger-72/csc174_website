183aa0552187133a7db38880d52b1c94daa4d9b4
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-11-26 13:36:08 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
-<?php
- include_once("index.html");
- $dbHost = "ec2-34-235-198-25.compute-1.amazonaws.com";
- $dbPort = "5432"
- $dbName = "d438kslsstlh9v";
- $dbuser = "ikovqwtdqjkzap";
- $dbpassword = "8d7d901015587862c8e4ca6f15e3ab3d91faea63fdb994e0045470b2c8f40981";   
+<html>
+    <head>
+        <title>Madrigal Employee DB</title>
+    </head>
+    <body>
+    <table>
+       <tr>
+        <th> WID </th>
+        <th> Location </th>
+       </tr>
+       <?php
+         $conn = pg_connect("host = ec2-34-235-198-25.compute-1.amazonaws.com  
+         port = 5432 
+         dbname = d438kslsstlh9v 
+         user = ikovqwtdqjkzap
+         password = 8d7d901015587862c8e4ca6f15e3ab3d91faea63fdb994e0045470b2c8f40981")
 
- $conn = pg_connect("host = ec2-34-235-198-25.compute-1.amazonaws.com  
-                     port = 5432 
-                     dbname = d438kslsstlh9v 
-                     user = ikovqwtdqjkzap
-                     password = 8d7d901015587862c8e4ca6f15e3ab3d91faea63fdb994e0045470b2c8f40981")
-  if($conn)
-  {
-    echo "connected";
-  }
-?>
+         if (!$conn) {
+            echo "An error occurred.\n";
+          }
+          else{
+            echo "Good\n";
+          }
+
+       ?>
+    <table>
+    </body>
+</html>
