48dde734274c300f0be6530161ed682db5ab81d5
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-11-26 13:43:37 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<html>
-    <head>
-        <title>Madrigal Employee DB</title>
-    </head>
-    <body>
-    <table>
-       <tr>
-        <th> WID </th>
-        <th> Location </th>
-       </tr>
-       <?php
-         $conn = pg_connect("host = ec2-34-235-198-25.compute-1.amazonaws.com  
-         port = 5432 
-         dbname = d438kslsstlh9v 
-         user = ikovqwtdqjkzap
-         password = 8d7d901015587862c8e4ca6f15e3ab3d91faea63fdb994e0045470b2c8f40981")
-
-         if (!$conn) {
-            echo "An error occurred.\n";
-          }
-          else{
-            echo "Good\n";
-          }
-
-       ?>
-    <table>
-    </body>
-</html>
+<php?
+      echo "test";
+      ?>
