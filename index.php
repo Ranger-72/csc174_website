01f130ac1e88252f4d56616b2d5f25a0d5613986
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-11-26 15:37:06 -0800

    Add files via upload
    Test to display Warehouse table

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-Test
+<?php
+include("config.php");
+$query = $conn->query("Select * from warehouse");
+?>
+
+<table border="1">
+<tr>
+    <td>WID</td>
+    <tc>Location</td>
+</tr>
+<?php
+while($row = $query->fetch_assoc()){
+    echo "<tr>
+    <td>{$row['warehouseID']}</td>
+    <td>{$row['location']}</td>
+    </tr";
+}
+?>
+</table>
