dad59bde6493b025a4f426ca01ddc54f8be33412
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-11-26 14:14:46 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-$conn = pg_connect("host=ec2-34-235-198-25.compute-1.amazonaws.com  
-                            port=5432 
-                            dbname=d438kslsstlh9v 
-                            user=ikovqwtdqjkzap
-                            password=8d7d901015587862c8e4ca6f15e3ab3d91faea63fdb994e0045470b2c8f40981")
-if (!$conn)
+$conn = pg_connect(getenv("postgres://ikovqwtdqjkzap:8d7d901015587862c8e4ca6f15e3ab3d91faea63fdb994e0045470b2c8f40981@ec2-34-235-198-25.compute-1.amazonaws.com:5432/d438kslsstlh9v"));
 {
     echo "Connection Failed";
 }
