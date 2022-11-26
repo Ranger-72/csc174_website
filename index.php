fc0866b1b137106c10aaa2d145d454563583540a
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-11-26 13:54:06 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-echo "Hello World";
+echo "Hello World\n";
+$conn = $conn = pg_connect("host=ec2-34-235-198-25.compute-1.amazonaws.com  
+                            port=5432 
+                            dbname=d438kslsstlh9v 
+                            user=ikovqwtdqjkzap
+                            password=8d7d901015587862c8e4ca6f15e3ab3d91faea63fdb994e0045470b2c8f40981")
+if (!$conn)
+{
+    echo "Connection Failed\n";
+}
+else {
+    echo "Connection Good\n";
+}
 ?>
