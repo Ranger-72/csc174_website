05603777be5367a0a73338059599fc16f57aa2a1
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-11-26 16:55:24 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
+<?php
+include_once("config.php");
+?>
+
 <form action = "submit.php" method = "POST">
     <input type = 'text' name = "location" placeholder = "Location">
-    <br>
+    <br><br>
     <button type = "submit" name = "submit">Submit</button>
     <br>
-    <br>
 </form>
+
+<table border="1">
+<tr>
+    <td>WID</td>
+    <td>Location</td>
+</tr>
+
+<?php
+$query = $conn->query("Select * from warehouse");
+while($row = $query->fetch_assoc()){
+    echo "<tr>
+    <td>{$row['warehouseID']}</td>
+    <td>{$row['location']}</td>
+    </tr>";
+}
+?>
+</table>
