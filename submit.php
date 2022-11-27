d37e16234789d2692dab926c0b6b233ee6c106bc
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-11-26 21:23:08 -0800

    Add files via upload

--- submit.php
+++ submit.php
@@ -1,15 +1,8 @@
-<table border="1" >
-<tr>
-    <td>WID</td>
-    <td>Location</td>
-</tr>
-</table>
-
 <?php
 include_once("config.php");
 
 $location = mysqli_real_escape_string($conn, $_POST['location']);
-$wID = '666';
+$wID = '754';
 
 $sql = "INSERT INTO warehouse (warehouseID, location) VALUES (?,?);";
 $stmt = mysqli_stmt_init($conn);
@@ -20,14 +13,21 @@ if (!mysqli_stmt_prepare($stmt,$sql)){
 else{
     mysqli_stmt_bind_param($stmt,"ss", $wID, $location);
     mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
+}
+?>
 
-    while($row = mysqli_fetch_assoc($result)){
+<table border="1" >
+<tr>
+    <td>WID</td>
+    <td>Location</td>
+</tr>
+<?php
+$query = $conn->query("Select * from warehouse");
+while($row = $query->fetch_assoc()){
     echo "<tr>
     <td>{$row['warehouseID']}</td>
     <td>{$row['location']}</td>
     </tr>";
 }
-}
 ?>
-
+</table>
