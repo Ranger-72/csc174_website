e141b8132ab0e1f8f52c868e90a2ee3336773c5e
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-11-26 20:58:22 -0800

    Prepared Statement

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<form action = "submit.php" method = "POST">
+<form action = "submit.php" target = "_blank" method = "POST">
     <input type = 'text' name = "location" placeholder = "Location">
     <br>
     <button type = "submit" name = "submit">Submit</button>
--- submit.php
+++ submit.php
@@ -1,32 +1,32 @@
+<table border="1" >
+<tr>
+    <td>WID</td>
+    <td>Location</td>
+</tr>
+</table>
+
 <?php
 include_once("config.php");
 
 $location = mysqli_real_escape_string($conn, $_POST['location']);
+$wID = '754';
 
-$sql = "INSERT INTO warehouse (warehouseID, location) VALUES (314,?);"
+$sql = "INSERT INTO warehouse (warehouseID, location) VALUES (?,?);"
 $stmt = mysqli_stmt_init($conn);
 
 if (!mysqli_stmt_prepare($stmt,$sql)){
     echo "Prepared Statement Failed";
 }
 else{
-    mysqli_stmt_bind_param($stmt,"s", $location);
-    mysqli_stmt_exexcute($stmt);
-}
-?>
+    mysqli_stmt_bind_param($stmt,"ss", $wID, $location);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
 
-<table border="1">
-<tr>
-    <td>WID</td>
-    <td>Location</td>
-</tr>
-<?php
-$query = $conn->query("Select * from warehouse");
-while($row = $query->fetch_assoc()){
-    echo "<tr>
-    <td>{$row['warehouseID']}</td>
-    <td>{$row['location']}</td>
-    </tr>";
+    while($row = mysqli_fetch_assoc($result)){
+        echo "<tr>
+        <td>{$row['warehouseID']}</td>
+        <td>{$row['location']}</td>
+        </tr>";
+    }
 }
 ?>
-</table>
