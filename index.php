9ea451f277c3bbb62f968fdd25744ab4a79ef036
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-11-26 21:08:04 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <form action = "submit.php" target = "_blank" method = "POST">
-    <input type = 'text' name = "location" placeholder = "Location">
+    <input type = 'text' name = "location" placeholder = "location">
     <br>
     <button type = "submit" name = "submit">Submit</button>
     <br>
