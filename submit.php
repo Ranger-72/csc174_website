d7146c3b995da2a814441f045e2947cdaeb7c619
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-11-26 21:12:40 -0800

    Update submit.php

--- submit.php
+++ submit.php
@@ -4,7 +4,7 @@ include_once("config.php");
 $location = mysqli_real_escape_string($conn, $_POST['location']);
 $wID = '754';
 
-$sql = "INSERT INTO warehouse (warehouseID, location) VALUES (?,?);"
+$sql = "INSERT INTO warehouse (warehouseID, location) VALUES (?,?);";
 $stmt = mysqli_stmt_init($conn);
 
 if (!mysqli_stmt_prepare($stmt,$sql)){
