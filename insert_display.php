d34965569c08beafb018fb553541bf22ae180f1b
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-12-02 13:14:19 -0800

    Add files via upload

--- insert_display.php
+++ insert_display.php
@@ -1,22 +1,3 @@
-<?php
-include_once("connection.php");
-
-$location = mysqli_real_escape_string($conn, $_POST['location']);
-$wID = '222';
-
-$sql = "INSERT INTO warehouse (warehouseID, location) VALUES (?,?);";
-$stmt = mysqli_stmt_init($conn);
-
-if (!mysqli_stmt_prepare($stmt,$sql)){
-    echo "Prepared Statement Failed";
-}
-else{
-    mysqli_stmt_bind_param($stmt,"ss", $wID, $location);
-    mysqli_stmt_execute($stmt);
-}
-mysqli_close($conn);
-?>
-
 <h1>Updated Data in Warehouse Table</h1>
 <table border="1" >
         <tr>
@@ -24,6 +5,23 @@ mysqli_close($conn);
             <td>Location</td>
         </tr>
         <?php
+        include_once("connection.php");
+
+        $location = mysqli_real_escape_string($conn, $_POST['location']);
+        $wID = '333';
+        
+        $sql = "INSERT INTO warehouse (warehouseID, location) VALUES (?,?);";
+        $stmt = mysqli_stmt_init($conn);
+        
+        if (!mysqli_stmt_prepare($stmt,$sql)){
+            echo "Prepared Statement Failed";
+        }
+        else{
+            mysqli_stmt_bind_param($stmt,"ss", $wID, $location);
+            mysqli_stmt_execute($stmt);
+        }
+        mysqli_close($conn);
+        
         include_once(select.php);
         ?>
 </table>
