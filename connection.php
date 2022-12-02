857568c7cd2e697e61285bad3ab9c507fa40a818
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-12-02 14:06:52 -0800

    Add files via upload

--- connection.php
+++ connection.php
@@ -5,11 +5,6 @@ $db_username = "bd89af7da4faac";
 $db_password = "0f0686de";
 $db_name = "heroku_524216f6fad8cbf";
 
-// create connection
-$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name) 
-
-// check if connection is successful
-if (!conn){
-  die ("Database connection error:" . mysqli_connect_error());
-}
+// create connection and check if connection is successful
+$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name) or die ("DB connection error");
 ?>
