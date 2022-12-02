d191b75f52401d08c4b45434488af59c533f0231
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-12-02 11:51:59 -0800

    Creating form and table on html and including html on index.php

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
-<form action = "submit.php" target = "_blank" method = "POST">
-    <input type = 'text' name = "location" placeholder = "location">
-    <br>
-    <button type = "submit" name = "submit">Submit</button>
-    <br>
-</form>
+<?php
+include_once(index.html);
+?>
 
-<table border="1" >
-        <tr>
-            <td>WID</td>
-            <td>Location</td>
-        </tr>
-        <?php
-        include_once("config.php");
-        $query = $conn->query("Select * from warehouse");
-        while($row = $query->fetch_assoc()){
-            echo "<tr>
-            <td>{$row['warehouseID']}</td>
-            <td>{$row['location']}</td>
-            </tr>";
-        }
-        mysqli_close($conn);
-        ?>
-</table>
+
