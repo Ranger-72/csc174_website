c6b1f3c17611f317a317b17309bce9588b3c0fda
Eduardo Mariano <92120071+Ranger-72@users.noreply.github.com>
2022-12-02 12:07:40 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
-<?php
-include_once(index.html);
-?>
+<form action = "submit.php" target = "_blank" method = "POST">
+    <input type = 'text' name = "location" placeholder = "location">
+    <br>
+    <button type = "submit" name = "submit">Submit</button>
+    <br>
+</form>
+
+
+<table border="1" >
+        <tr>
+            <td>WID</td>
+            <td>Location</td>
+        </tr>
+        <?php
+        include_once("config.php");
+        $query = $conn->query("Select * from warehouse");
+        while($row = $query->fetch_assoc()){
+            echo "<tr>
+            <td>{$row['warehouseID']}</td>
+            <td>{$row['location']}</td>
+            </tr>";
+        }
+        mysqli_close($conn);
+        ?>
+</table>
 
 
